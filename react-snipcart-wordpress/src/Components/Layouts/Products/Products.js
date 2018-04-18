3b0efa51553032e0deed6b584606e396a54ee7ed
Travis1282 <travisjosephclark@gmail.com>
2018-04-17 23:05:54 -0500

    product grid

--- react-snipcart-wordpress/src/Components/Layouts/Products/Products.js
+++ react-snipcart-wordpress/src/Components/Layouts/Products/Products.js
@@ -4,8 +4,13 @@ import Typography from 'material-ui/Typography';
 import AddtoCart from './Buttons/AddtoCart';
 import Image from './Media/Image';
 import Description from './Text/Description';
+import Grid from 'material-ui/Grid';
+import PropTypes from 'prop-types';
 import './Products.css';
 
+function AutoGrid(props) {
+  const { classes } = props;
+}
 
  export default class Products extends React.Component {
 	render(){
@@ -13,25 +18,29 @@ import './Products.css';
 			<div className="products">
 		      {this.props.AllProductData.products.map((product) => 
 	            <div className="product" key={` product-${product.id}}`}>
-	              <Card>
-			        <CardContent>
-				        <CardMedia>
-				        	<Image product={product} />
-				        </CardMedia>
-				        <Typography variant="headline" component="h2">
-				        	{ product.name }
-				        </Typography>					          
-				        <Typography gutterBottom variant="subheading" component="h2">
-				         	{ product.tagline } 
-				        </Typography>
-				        <Typography component="p">
-				        	<Description product={product} />
-				        </Typography>
-			        </CardContent>
-			        <CardActions>
-			        	<AddtoCart product={product} />
-			        </CardActions>
-				  </Card>
+	            	<Grid container spacing={24}>
+				      <Grid item xs>
+			              <Card>
+					        <CardContent>
+						        <CardMedia>
+						        	<Image product={product} />
+						        </CardMedia>
+						        <Typography variant="headline" component="h2">
+						        	{ product.name }
+						        </Typography>					          
+						        <Typography gutterBottom variant="subheading" component="h2">
+						         	{ product.tagline } 
+						        </Typography>
+						        <Typography component="p">
+						        	<Description product={product} />
+						        </Typography>
+					        </CardContent>
+					        <CardActions>
+					        	<AddtoCart product={product} />
+					        </CardActions>
+						  </Card>
+						</Grid>
+					</Grid>
 				</div>
 	          )}
 			</div>
